f9048ab08aabe2b5bf6857e3d8215224750ff0e0
cgozzard <cgozzard@yahoo.com>
2019-03-02 20:50:35 +0200

    Update Wemos.ino

--- Wemos.ino
+++ Wemos.ino
@@ -22,16 +22,16 @@
 #define D9 13 // GPIO2 maps to Ardiuno D9
 #define D10 15 // GPIO15 maps to Ardiuno D10
 
-const char* ssid     = "ssid goes here";      // SSID of local network
-const char* password = "password goes here";   // Password on network
-String APIKEY = "API Key goes here"; // see https://openweathermap.org/appid
+const char* ssid     = "";      // SSID of local network
+const char* password = "41394342";   // Password on network
+String APIKEY = ""; // Get a API key from openweathermap.org
 //String CityID = "6942529"; //Durbanville / ZA
-String CityID = "3369157"; //Cape Town / ZA Go to https://openweathermap.org/city/ and insert city name - if found number is now in URL 
-
+String CityID = "3369157"; //Cape Town / ZA
+const char* CallSign = ""; //Your amateur radio  callsign
 
 WiFiClient client;
 char servername[]="api.openweathermap.org";  // remote server we will connect to
-char server[]="www.hamqsl.com";// remote server we will connect to get ham date
+char server[]="www.hamqsl.com";
 String result;
 
 int  counter = 60;
@@ -46,6 +46,8 @@ float Kvalue;
 float Avalue;
 float SFIvalue;
 float Sunspots;
+float Wind;
+float WindAngle;
 
 LiquidCrystal lcd(D8,D9,D4,D5,D6,D7); 
 
@@ -53,6 +55,11 @@ void setup() {
   Serial.begin(115200);
   int cursorPosition=0;
   lcd.begin(16, 2);
+  lcd.print("   ");
+  lcd.print(CallSign);
+  lcd.print("   ");
+  delay (500);
+  lcd.clear();
   lcd.print("   Connecting");  
   Serial.println("Connecting");
   WiFi.begin(ssid, password);
@@ -96,6 +103,8 @@ void loop() {
       displayConditions(Temperature,Humidity,Pressure);
       delay(5000);
       displayTime();
+      delay(5000);
+      displayWind(Wind, WindAngle);
       delay(5000);
       displayHam(Kvalue,Avalue,SFIvalue,Sunspots);
       delay(5000);
@@ -173,6 +182,8 @@ float humidity = root["main"]["humidity"];
 String weather = root["weather"]["main"];
 String description = root["weather"]["description"];
 float pressure = root["main"]["pressure"];
+float wind = root["wind"]["speed"];
+float windAngle = root["wind"]["deg"];
 
 weatherDescription = description;
 weatherLocation = location;
@@ -180,7 +191,8 @@ Country = country;
 Temperature = temperature;
 Humidity = humidity;
 Pressure = pressure;
-
+WindAngle = windAngle;
+Wind = wind;
 }
 
 void displayWeather(String location,String description)
@@ -198,19 +210,19 @@ void displayConditions(float Temperature,float Humidity, float Pressure)
 {
   lcd.clear();
   lcd.print("T:"); 
- lcd.print(Temperature,1);
+ lcd.print(Temperature,0);
  lcd.print((char)223);
  lcd.print("C ");
  
  //Printing Humidity
  lcd.print(" H:");
  lcd.print(Humidity,0);
- lcd.print(" %");
+ lcd.print("%");
  
  //Printing Pressure
  lcd.setCursor(0,1);
  lcd.print("P: ");
- lcd.print(Pressure,1);
+ lcd.print(Pressure,0);
  lcd.print(" hPa");
 
 }
@@ -228,6 +240,19 @@ void displayTime()
   time_t now = time(nullptr);
   lcd.println(ctime(&now));;
 }
+
+void displayWind(float wind,float windAngle)
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Wind:");
+  lcd.print(wind,0);
+  lcd.print("m/s");
+  lcd.setCursor(0,1);
+  lcd.print("Direction:");
+  lcd.print(windAngle,0);
+  lcd.print((char)223);
+} 
 
 
 void displayHam(float Kvalue, float Avalue, float SFIvalue, float Sunspots)
@@ -248,31 +273,31 @@ void displayHam(float Kvalue, float Avalue, float SFIvalue, float Sunspots)
 void displayBands (float SFIvalue){
   lcd.clear();
   lcd.setCursor(0,0);
-  if (SFIvalue > 64 && SFIvalue < 70 )
+  if (SFIvalue > 64 && SFIvalue < 71 )
   {
   lcd.print("Bands above 40m");
   lcd.setCursor(0,1);
   lcd.print("unusable");
   }
-  if (SFIvalue > 70 && SFIvalue < 90 )
+  if (SFIvalue > 70 && SFIvalue < 91 )
   {
   lcd.print("all bands up ");
   lcd.setCursor(0,1);
   lcd.print("through 20m");
   }
-  if (SFIvalue > 90 && SFIvalue < 120 )
+  if (SFIvalue > 90 && SFIvalue < 121 )
   {
   lcd.print("Fair conditions");
   lcd.setCursor(0,1);
   lcd.print("up through 15m");
   }
-  if (SFIvalue > 120 && SFIvalue < 150 )
+  if (SFIvalue > 120 && SFIvalue < 151 )
   {
   lcd.print("good conditions ");
   lcd.setCursor(0,1);
   lcd.print("up through 10m");
   }
-  if (SFIvalue > 150 && SFIvalue < 200 )
+  if (SFIvalue > 150 && SFIvalue < 201 )
   {
   lcd.print("Excellent up");
   lcd.setCursor(0,1);
